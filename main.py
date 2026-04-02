f8d1d34149eaeb104acd5829172cd2e4cda23021
cirko0 <cirko007ivan@gmail.com>
2026-04-02 14:11:16 +0200

    feat: create add_task function and store tasks in json file

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
+import sys
+import json
+from pathlib import Path
+
+file = Path("data.json")
 
 def add_task(task):
-    print("add task")
+    content = {
+        "name": task,
+        "status": "in progress"
+    }
+
+    if file.is_file():
+        with open("data.json", "rb+") as f:
+            size = f.tell()
+            f.seek(size-1, 2)
+            f.truncate()  # delete the last char
+            f.write((", " + json.dumps(content) + "]").encode("utf-8"))
+    else:
+        with open("data.json", "w") as f:
+            f.write("[" + json.dumps(content) + "]")
 
 if __name__ == '__main__':
     user_input = input("Please enter your task: ")
